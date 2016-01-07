b258181fa6872877a391636ed023f15f2c533d45
Florence Loi <florence.c.loi@gmail.com>
2016-01-07 12:45:18 -0800

    Check if enough numbers inputted

--- calculator.py
+++ calculator.py
@@ -20,34 +20,36 @@ while True:
         print "That's not a valid command. Try again!"
         break
 
+    operators = ["+", "-", "*", "/", "pow", "mod", "square", "cube"]
+
     if len(token) == 1:
         if token[0] == "q":
             break
-
-    operators = ["+", "-", "*", "/", "square", "cube", "pow", "mod"]
-
-    if token[0] in operators:
-        # if len(token) >= 3 and (token[0] == "+" or token[0] == "-" or token[0] == "*" or token[0] == "/"): 
-        if token[0] == "+":
-            print add(token)
-        elif token[0] == "-":
-            print subtract(token)
-        elif token[0] == "*":
-            print multiply(token)
-        elif token[0] == "/":
-            print divide(token)
-        # elif len(token) == 2 and (token[0] == "square" or token[0] == "cube"):
-        elif token[0] == "square":
-            print square(token)
-        elif token[0] == "cube":
-            print cube(token)
-        # elif len(token) == 3 and (token[0] == "pow" or token[0] == "mod"):
-        elif token[0] == "pow":
-            print power(token)
-        elif token[0] == "mod":
-            print mod(token)
+    elif len(token) > 1:
+        if token[0] in operators:
+            if token[0] in operators[:6]: 
+                if len(token) >= 3:
+                    if token[0] == "+":
+                        print add(token)
+                    elif token[0] == "-":
+                        print subtract(token)
+                    elif token[0] == "*":
+                        print multiply(token)
+                    elif token[0] == "/":
+                        print divide(token)
+                    elif token[0] == "pow":
+                        print power(token)
+                    elif token[0] == "mod":
+                        print mod(token)
+                else:
+                    print "Not enough numbers"
+            if token[0] in operators[6:]:
+                if token[0] == "square":
+                    print square(token)
+                elif token[0] == "cube":
+                    print cube(token)
+        else:
+            print "Operator not valid."
     else:
-        print "Operator not valid."
-
-
+        print "That's not a valid command. Try again!"
 
